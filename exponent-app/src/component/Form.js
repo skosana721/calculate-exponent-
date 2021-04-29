13e2d2750952061b0162b79ca81fb2b9ae7a0d9e
skosana721 <wonganiskosana@gmail.com>
2021-04-29 09:53:21 +0200

    deconstructed the inputNumber state and passed teh value in the input

--- exponent-app/src/component/Form.js
+++ exponent-app/src/component/Form.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 
 function Form() {
   const [inputNumber, setInputNumber] = useState({ base: "", exponent: "" });
+  const { base, exponent } = inputNumber;
   return (
     <div>
       <form>
         <label htmlFor="base">Base</label>
-        <input type="number" name="base" />
+        <input type="number" name="base" value={base} />
         <label htmlFor="exponent">Exponent</label>
-        <input type="number" name="exponent" />
+        <input type="number" name="exponent" value={exponent} />
         <button type="submit">Calculate</button>
       </form>
     </div>
